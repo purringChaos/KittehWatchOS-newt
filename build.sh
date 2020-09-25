a22b68c96fea45e19edfa8e48cbbcffce5889fa3
namedkitten <kitteh@kitteh.pw>
2020-09-25 12:01:55 +0100

    uwu

---
+++ build.sh
@@ -0,0 +1,3 @@
+newt build -p KittehWatchOS-newt
+newt create-image KittehWatchOS-newt 69
+scp bin/targets/KittehWatchOS-newt/app/apps/KittehWatchOS-newt/KittehWatchOS-newt.img root@192.168.1.64:/Websites/owo.monster/
